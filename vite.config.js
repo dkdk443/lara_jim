ae6b3bd5ddd4e3f1e074282bf3676fcbf4a315e7
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2022-07-04 20:49:41 +0900

    laravel breeze導入

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-        }),
-        react(),
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
     ],
 });
